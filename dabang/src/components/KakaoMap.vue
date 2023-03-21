6272c57f03c68662c78009eced8470f555456209
박도영 <pdyme1207@gmail.com>
2023-03-21 16:31:45 +0900

    modal rolling

--- dabang/src/components/KakaoMap.vue
+++ dabang/src/components/KakaoMap.vue
@@ -1,13 +1,111 @@
 <template>
   <div>
     <!-- <h2>카카오 맵 보기</h2> -->
-    <div id="map"></div>
+    <div id="map">
+      <div class="rollingbanner">
+        <div class="title">속보 ></div>
+        <div class="wrap">
+          <ul>
+            <li><a href="#">노바백스 백신 2월중순부터 접종</a></li>
+            <li class="next">
+              <a href="#">얼어붙은 투심에…현대엔지니어링 상장 철회</a>
+            </li>
+            <li class="current">
+              <a href="#"
+                >"일본 정부, 사도광산 세계유산 추천 방침 굳혀, 일본과 갈등
+                첨예화 예상"</a
+              >
+            </li>
+            <li>
+              <a href="#">"공법변경 구조검토 요구, 현산 측이 묵살했다"</a>
+            </li>
+            <li class="prev">
+              <a href="#">12월 주담대 금리 연 3.63%…7년7개월 만에 최고</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-<style scope>
+<style lang="scss" scope>
 #map {
   width: 100%;
   height: 100vh;
+  overflow: hidden;
+  position: fixed;
+}
+$font-color: rgb(160, 202, 255);
+.rollingbanner {
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  width: 450px;
+  height: 60px;
+  padding: 9px 25px 8px 9px;
+  border: 1px solid rgba(12, 18, 28, 0.92);
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 12px 25px 0px;
+  // background-color: rgba(39, 44, 52, 0.92);
+  background-color: white;
+  position: absolute;
+  right: 580px;
+  bottom: 30px;
+  z-index: 300;
+  overflow: hidden;
+}
+
+/* 타이틀 */
+.rollingbanner > .title {
+  font-weight: bold;
+  float: left;
+  padding-right: 10px;
+  color: $font-color;
+}
+/* 롤링 배너 */
+.rollingbanner > .wrap {
+  position: relative;
+  width: 80%;
+  height: 50%;
+  box-sizing: border-box;
+  overflow: hidden;
+}
+.rollingbanner ul {
+  list-style: none;
+}
+.rollingbanner li {
+  position: absolute;
+  top: -36px;
+  left: 0;
+}
+/* 이전, 현재, 다음 롤링 배너 표시 */
+.rollingbanner li.prev {
+  top: 36px;
+  transition: top 0.5s ease;
+}
+.rollingbanner li.current {
+  top: 0;
+  transition: top 0.5s ease;
+}
+.rollingbanner li.next {
+  top: -36px;
+}
+.rollingbanner a {
+  display: block;
+  display: -webkit-box;
+  text-decoration: none;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  color: $font-color !important;
+}
+/* 반대 방향으로 진행 */
+.rollingbanner.reverse li.prev {
+  top: -36px;
+  transition: top 0.5s ease;
+}
+.rollingbanner.reverse li.next {
+  top: 36px;
 }
 </style>
 
@@ -21,7 +119,12 @@ export default {
     };
   },
   setup() {},
-  created() {},
+  created() {
+    setTimeout(() => {
+      console.log("this", this);
+      this.sayHi();
+    }, 1000);
+  },
   mounted() {
     // api 스크립트 소스 불러오기 및 지도 출력
     if (window.kakao && window.kakao.maps) {
@@ -32,7 +135,12 @@ export default {
   },
   unmounted() {},
   methods: {
+    sayHi() {
+      console.log("sayHi");
+    },
     // api 불러오기
+    //localhost : d6f7f63094ceaef6d6ee6e0a2d218dbd
+    //server : 1914d03923dca3643125bf3c5ebc8d58
     loadScript() {
       const script = document.createElement("script");
       script.src =
@@ -46,8 +154,8 @@ export default {
       const container = document.getElementById("map");
       const options = {
         center: new window.kakao.maps.LatLng(
-          36.351229560970026,
-          127.38723344758682
+          36.3533618250382,
+          127.40194425812012
         ),
         level: 5,
       };
@@ -238,4 +346,45 @@ export default {
     },
   },
 };
+var interval;
+document.addEventListener("DOMContentLoaded", () => {
+  // 롤링 초기화
+  interval = window.setInterval(rollingCallback, 3000);
+
+  //마우스 호버시 롤링이 멈추었다 벗어나면 다시 롤링이 되도록 처리
+  document
+    .querySelector(".rollingbanner")
+    .addEventListener("mouseover", function () {
+      window.clearInterval(interval);
+    });
+  document
+    .querySelector(".rollingbanner")
+    .addEventListener("mouseout", function () {
+      interval = window.setInterval(rollingCallback, 1000);
+    });
+});
+
+function rollingCallback() {
+  //.prev 클래스 삭제
+  document.querySelector(".rollingbanner .prev").classList.remove("prev");
+
+  //.current -> .prev
+  let current = document.querySelector(".rollingbanner .current");
+  current.classList.remove("current");
+  current.classList.add("prev");
+
+  //.next -> .current
+  let next = document.querySelector(".rollingbanner .next");
+  //다음 목록 요소가 널인지 체크
+  if (next.nextElementSibling == null) {
+    document
+      .querySelector(".rollingbanner ul li:first-child")
+      .classList.add("next");
+  } else {
+    //목록 처음 요소를 다음 요소로 선택
+    next.nextElementSibling.classList.add("next");
+  }
+  next.classList.remove("next");
+  next.classList.add("current");
+}
 </script>
