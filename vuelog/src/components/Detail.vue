47b0e7094dd19ac4fe74c7c2d7b5431438fab51e
박도영 <pdyme1207@gmail.com>
2023-03-24 14:08:25 +0900

    Vuestagram 🧑‍💼

--- vuelog/src/components/Detail.vue
+++ vuelog/src/components/Detail.vue
@@ -2,7 +2,7 @@
   {{ $route.params.id }}
   <div class="container">
     <h4>안녕하세요? 상세페이지입니다.</h4>
-    <h5>{{ vlog[$route.params.id].title }}</h5>
+    <h5 @click="$router.go(-1)">{{ vlog[$route.params.id].title }}</h5>
     <p>{{ vlog[$route.params.id].content }}</p>
     <router-view></router-view>
   </div>
--- vuelog/src/components/List.vue
+++ vuelog/src/components/List.vue
@@ -23,4 +23,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+h5 {
+  cursor: pointer;
+}
+</style>
